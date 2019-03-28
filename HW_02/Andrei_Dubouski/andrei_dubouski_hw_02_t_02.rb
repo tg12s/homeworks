489b837c7a2da74dd7011d7efd4d6785828f8364
Dubouski Andrei <14234013+Dubouski@users.noreply.github.com>
2019-03-28 11:10:05 +0300

    Dubouski  Homework 02 (#17)

---
+++ HW_02/Andrei_Dubouski/andrei_dubouski_hw_02_t_02.rb
@@ -0,0 +1,6 @@
+puts 'The equation to get 24 with 1,3,4 and 6 with only /, * and +'
+puts 'So...'
+result = (6 / 3 + 1) * (1 + 4 + 3) / 4 * (3 + 1)
+puts '(6 / 3 + 1) * (1 + 4 + 3) / 4 * (3 + 1) is:'
+puts result
+puts 'Mmmm..., it was interested'
