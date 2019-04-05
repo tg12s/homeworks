56fe75fdf9877f93f86167e8092f6f5fa4070514
labs-ruby <48762237+labs-ruby@users.noreply.github.com>
2019-04-05 15:36:14 +0300

    Add specs for 4th hw (#82)

--- HW_04/test_helper.rb
+++ HW_04/test_helper.rb
@@ -1,26 +1,45 @@
-require 'minitest/pride'
-
+# require 'pry'
 # rubocop:disable all
 class HW4Runner
   class << self
     def call
-      tasks_range.each do |task_number|
-        next unless path_for_task(task_number)
-        require path_for_task(task_number)
+      @errors = Hash.new
+
+      directories.each do |dir|
+        Process.fork do
+          tasks_in(dir).each do |task|
+            require hw_path(dir, task)
+          end
+          puts "---- Running tests for #{dir} ----"
+          require './HW_04/specs_4.rb'
+        end
+        Process.wait
+        @errors[dir] = $?.exitstatus
       end
-      require File.join('.', File.dirname(__FILE__), "/specs/main_spec.rb")
+
+      exit_on_failure!(@errors)
     end
 
     private
 
-    def tasks_range
-      (1..4).to_a(&:to_s)
+    def exit_on_failure!(errors)
+      if errors.values.count { |v| v == 1 } > 0
+        puts errors
+        puts "Some specs exited with 1, exiting..."
+        exit 1
+      end
     end
 
-    def path_for_task(number)
-      regexp = Regexp.new("hw_04_t_0#{number}")
-      exclude_maks_regexp = Regexp.new("maksim_holubeu")
-      Dir[File.join('.', '**/*.rb')].select { |f| f.match?(regexp) && !f.match?(exclude_maks_regexp) }.last
+    def hw_path(dir, filename)
+      File.join("./", dir, filename)
+    end
+
+    def directories
+      Dir.glob('HW_04/*').select { |f| File.directory? f }
+    end
+
+    def tasks_in(folder)
+      Dir.entries(folder).select { |file| file.match? %{hw_04.*\.rb} }
     end
   end
 end
