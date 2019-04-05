7c099f57cfad831e3b454eaf4ee0f7daddd9ad82
labs-ruby <48762237+labs-ruby@users.noreply.github.com>
2019-04-05 15:05:24 +0300

    Revert "(#33)" (#81)
    This reverts commit 8ad04425afdbfba9ed06feb6b5b98fa697be20ac.

--- HW_03/Vasili Makouchyk/vasili_makouchyk_hw_03_t_01.rb
+++
@@ -1,5 +0,0 @@
-def task_1(text)
-  text.lines.each do |string|
-    return string if string.downcase.include?('error')
-  end
-end
--- HW_03/Vasili Makouchyk/vasili_makouchyk_hw_03_t_04.rb
+++
@@ -1,3 +0,0 @@
-def task_4(text)
-  text.delete('^0-9').chars.inject(0) { |sum, x| sum + x.to_i }
-end
